2e602e15aea03d8b94921bfd9414694bf37e0ba2
Berkay Çubuk <berkaycubuk@gmail.com>
2021-04-21 17:01:42 +0300

    32 bit mode enabled

--- boot.asm
+++ boot.asm
@@ -1,5 +1,71 @@
-jmp $
+bits 16                         ; 16 bit mode
+org 0x7c00                      ; bootloader address
 
-times 510-($-$$) db 0
+boot:
+    mov ax, 0x2401
+    int 0x15                    ; enable A20 bit
+    ; set the VGA text mode to known value to be safe
+    ;mox ax, 0x3
+    ;int 0x10                    ; set vga text mode 3
+    cli
+    lgdt [gdt_pointer]          ; load the gdt table
+    mov eax, cr0
+    or eax, 0x1                 ; set cr0 for protected mode bit
+    mov cr0, eax
+    jmp CODE_SEG:boot2          ; long jump to the code segment
 
-db 0x55, 0xaa
+gdt_start:
+    dq 0x0
+
+gdt_code:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10011010b
+    db 11001111b
+    db 0x0
+
+gdt_data:
+    dw 0xFFFF
+    dw 0x0
+    db 0x0
+    db 10010010b
+    db 11001111b
+    db 0x0
+
+gdt_end:
+gdt_pointer:
+    dw gdt_end - gdt_start
+    dd gdt_start
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
+bits 32                         ; 32 bit mode
+boot2:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov esi, hello
+    mov ebx, 0xb8000
+
+.loop
+    lodsb
+    or al, al
+    jz halt
+    or eax, 0x0100
+    mov word [ebx], ax
+    add ebx, 2
+    jmp .loop
+    
+halt:
+    cli                         ; clear interrupt flag
+    hlt                         ; halt execution
+
+hello: db "Hello world", 0
+
+times 510-($-$$) db 0           ; fill remaining bytes with 0
+dw 0xaa55                       ; magic number
